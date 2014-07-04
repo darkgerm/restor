a86762c91bdea4117e2190767b17694dc48cb243
darkgerm <darkgerm@gmail.com>
2014-07-04 11:51:37 +0800

    Fix naming and syntax of ini_parser.py

--- ini_parser.py
+++ ini_parser.py
@@ -1,15 +1,15 @@
 import configparser
 
-def iniParser(inputFile):
+def ini_parser(input_file):
     """
         input parameter: file name. eg: test.ini
         output type: list
 
-        usage: iniParset(inputFIle.ini)
+        usage: iniParset(input_file.ini)
     """
     config = configparser.ConfigParser()
     config.optionxform = str
-    config.read(inputFile)
+    config.read(input_file)
     section_dict = {}
     output_list = []
     method = ""
@@ -25,7 +25,7 @@ def iniParser(inputFile):
         specific_key = "" 
         # set json_url ( json OR urlencode )
         for item in config.items(sSection):
-            if is_GET == True and item[0]=="input_format":
+            if is_GET == True and item[0] == "input_format":
                 raise Exception("Error: GET with 'input_format'.")
             if item[0] == "input_type" and item[1] == "urlencode":
                 json_url = "urlencode"
@@ -33,7 +33,7 @@ def iniParser(inputFile):
                 json_url = "json"
         for item in config.items(sSection):
             key = temp_key = item[0]
-            value = item[1].replace("\n","")
+            value = item[1].replace("\n", "")
             # handle specific case: urlencode OR json
             if key == "input_format" and json_url == "urlencode":
                 temp_value = urlencode_format(value)
@@ -45,21 +45,16 @@ def iniParser(inputFile):
                 temp_value = url_params_format(value)
             else: 
                 temp_value = value
-            section_dict.update({temp_key:temp_value})
-        section_dict.update({'method':method})
-        section_dict.update({'api':api})
+            section_dict.update({temp_key: temp_value})
+        section_dict.update({'method': method})
+        section_dict.update({'api': api})
         output_list.append(section_dict.copy())
         section_dict.clear()
     return output_list
 
 # json_format return json-like string
 def json_format(value):
-    value = value.replace("\n","")
-    return value
-
-# url_params_format return json-like string
-def url_params_format(value):
-    
+    value = value.replace("\n", "")
     return value
 
 # headers_format return dictionary
@@ -68,7 +63,7 @@ def headers_format(value):
     value_list = value.split("\n")
     for i in value_list:
         data = i.split(":")
-        output_value.update({data[0]:data[1].strip()}) 
+        output_value.update({data[0]: data[1].strip()}) 
     return output_value
 
 # url_params_format return dictionary
@@ -80,8 +75,8 @@ def url_params_format(value):
         key = pair[0]
         if pair[0] != "":
             temp_value = pair[1]
-            temp_value = temp_value.replace("(","")
-            temp_value = temp_value.replace(")","")
+            temp_value = temp_value.replace("(", "")
+            temp_value = temp_value.replace(")", "")
             value_list = temp_value.split(",")
             first = value_list[0]
             second = value_list[1]
@@ -97,21 +92,21 @@ def url_params_format(value):
                 else:
                     second = bool(second)
             # empty means essential, essential is True
-            output_value.update({key:(first, second)})
+            output_value.update({key: (first, second)})
     return output_value
 
 
 # urlencode_format return dictionary
 def urlencode_format(value):
-    value = value.replace(")",") ")
+    value = value.replace(")", ") ")
     output_value = {}
     for i in value.split(" "):
         pair = i.split("=")
         key = pair[0]
         if pair[0] != "":
             temp_value = pair[1]
-            temp_value = temp_value.replace("(","")
-            temp_value = temp_value.replace(")","")
+            temp_value = temp_value.replace("(", "")
+            temp_value = temp_value.replace(")", "")
             value_list = temp_value.split(",")
             first = value_list[0]
             second = value_list[1]
