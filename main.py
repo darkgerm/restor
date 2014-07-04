01c4eae7f3e2ab65a1a14654aabe245171f40585
darkgerm <darkgerm@gmail.com>
2014-07-04 11:27:12 +0800

    First version of server.

---
+++ main.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import urllib.parse
+import os
+import os.path
+
+from flask import Flask
+from flask import abort
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+app = Flask(__name__)
+
+import requests
+
+# logger shortcut
+debug = app.logger.debug
+warn = app.logger.warning
+error = app.logger.error
+
+
+#################### settings ####################
+HOST, PORT = '0.0.0.0', 8000
+UPLOAD_DIR = './upload/'
+DEBUG = True
+
+
+#################### utils ####################
+def parse_headers(headers):
+    """Multi-line string of headers -> dict"""
+    rst = {}
+    for line in headers.split('\n'):
+        entry = line.split(':')
+        if len(entry) < 2: continue
+        rst.update({entry[0].strip(): entry[1].strip()})
+    return rst
+
+
+#################### routing ####################
+@app.route('/hello/<name>', methods=['GET'])
+def hello_get(name):
+    return render_template('hello.html')
+
+
+@app.route('/hello/<name>', methods=['POST'])
+def hello_post(name):
+    #print(request.data)
+    #print(request.form)
+    #print(request.headers)
+    data = dict(urllib.parse.parse_qsl(request.data.decode()))
+    return 'haha ' + name + ' : ' + data['name']
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/action', methods=['POST'])
+def action():
+    url = request.form['url']
+    method = request.form['method']
+    headers = request.form.get('headers', '')
+    data = request.form.get('data', '')
+
+    if   method == 'GET':       query_func = requests.get
+    elif method == 'POST':      query_func = requests.post
+    elif method == 'PUT':       query_func = requests.put
+    elif method == 'DELETE':    query_func = requests.delete
+    else:
+        return 'Unknown method.', 500
+
+    headers = parse_headers(headers)
+
+    print(headers)
+    print(data)
+    r = query_func(url, data=data, headers=headers)
+    
+    return render_template(
+        'action.html',
+        status_code = r.status_code,
+        headers = r.headers,
+        data = r.text,
+    )
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if not file:
+            return render_template(
+                'upload.html',
+                error='Please select a valid file.'
+            )
+
+        filename = request.form['filename']
+        full_name = os.path.join(UPLOAD_DIR, filename)
+        if os.path.exists(full_name):
+            return render_template(
+                'upload.html',
+                error='File already exists.'
+            )
+        file.save(full_name)
+        return redirect(url_for('index'))
+
+
+#################### main ####################
+def main():
+    if not os.path.exists(UPLOAD_DIR):
+        os.mkdir(UPLOAD_DIR)
+
+    app.run(
+        host = HOST, port = PORT,
+        threaded = True,
+        debug = DEBUG,
+    )
+
+if __name__ == '__main__':
+    main()
